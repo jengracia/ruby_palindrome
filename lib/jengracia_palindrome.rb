d95c693f53bdb47c177852ed141f1d8f9e1eaa40
Judith Engracia <judith.engracia@gmail.com>
2018-12-26 12:31:04 -0700

    Include palindrome module in Integer class

--- lib/jengracia_palindrome.rb
+++ lib/jengracia_palindrome.rb
@@ -1,16 +1,29 @@
 require "jengracia_palindrome/version"
 
-class String
+module JengraciaPalindrome
+
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+  include JengraciaPalindrome
+end
+
+class Integer
+  include JengraciaPalindrome
 end
